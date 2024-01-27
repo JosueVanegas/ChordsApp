d5aead0e1d7d7551f5c28f65d6644cc92e087df7
JosueVanegas <josuerocha461@gmail.com>
2024-01-27 13:25:39 -0600

    changing to vite en create the proyecto just with react astro y not good for the proyect

--- client/src/components/SongCardMin.jsx
+++ client/src/components/SongCardMin.jsx
@@ -21,7 +21,7 @@ const SongCardMin = ({
         </div>
       </div>
       <div className="flex justify-center items-center">
-        <a href={`/song/${id}`}>
+        <a href={`/songs/${id}`}>
           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-5 rounded">
             view
           </button>
